80ce977051a644ee7750a62510259a7d6acb9879
Lakkakula Baba Srikanth <100520235+babasrikanth9988@users.noreply.github.com>
2024-03-06 19:28:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"  # Specify your desired AWS region
-  access_key = "AKIAVRUVQJXG2TVTTBLZ"
-  secret_key = "jRZyxr3k3VoipfMC/dgVUZfhzs5BZnk88p7206lL"
+  access_key = "AKIAVRUVQJXG2TVTTBLZl"
+  secret_key = "jRZyxr3k3VoipfMC/dgVUZfhzs5BZnk88p7206lLb"
 }
